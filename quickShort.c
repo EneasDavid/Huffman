2fa37ff36056faff2ba7cc32c0d0f1af94d4e72c
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-08-05 18:01:21 -0300

    Atualizar o quickShort.c

---
+++ quickShort.c
@@ -0,0 +1,18 @@
+void quick_sort(int *v, int size){
+ if (size <= 1) { 
+    return;
+ } else {
+ int pivot = v[size / 2];
+ int a = 0;
+ int b = size - 1;
+ 
+ while (a < b) {
+ while (v[a] < pivot) a++;
+ while (v[b] > pivot) b--;
+ if (a < b)
+ swap(&v[a], &v[b]);
+ }
+ quick_sort(v, b);
+ quick_sort(v+a, size - a);
+}
+}
